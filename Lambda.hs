66526c4d62e243df2cc6334b9d8fbcee0a8cd757
olligobber <sirolligobbervii@yahoo.com.au>
2021-01-25 03:18:09 +1100

    Tidied DeBruijn index type and started adding instances to lambda

--- Lambda.hs
+++ Lambda.hs
@@ -9,18 +9,43 @@ module Lambda
 	) where
 
 import Data.Void (Void)
-import Data.List (genericIndex)
 
 import BinaryTree (BinaryTree(..), renderL)
 import Reducible (Appliable(..))
 
-data LambdaTerm v = Abstraction (Lambda v) | Bound Integer | Free v
-	deriving (Eq, Ord, Show) -- todo remove show
+data LambdaTerm v = Abstraction (Lambda v) | Bound Int | Free v
+	deriving (Eq, Ord)
 
-newtype Lambda v = Lambda { getTree :: BinaryTree (LambdaTerm v) }
-	deriving (Eq, Ord, Show) -- todo remove show
+instance Functor LambdaTerm where
+	fmap f (Abstraction l) = Abstraction $ f <$> l
+	fmap _ (Bound i) = Bound i
+	fmap f (Free x) = Free $ f x
+
+instance Foldable LambdaTerm where
+	foldMap f (Abstraction l) = foldMap f l
+	foldMap _ (Bound _) = mempty
+	foldMap f (Free x) = f x
+
+instance Traversable LambdaTerm where
+	sequenceA (Abstraction l) = Abstraction <$> sequenceA l
+	sequenceA (Bound i) = pure $ Bound i
+	sequenceA (Free x) = Free <$> x
+
+newtype Lambda v = Lambda (BinaryTree (LambdaTerm v))
+	deriving (Eq, Ord)
 
 type LambdaCombinator = Lambda Void
+
+instance Functor Lambda where
+	fmap f (Lambda x) = Lambda $ fmap f <$> x
+
+instance Foldable Lambda where
+	foldMap f (Lambda x) = foldMap (foldMap f) x
+
+instance Traversable Lambda where
+	sequenceA (Lambda x) = Lambda <$> traverse sequenceA x
+
+-- todo applicative and monad instances
 
 instance Appliable (Lambda v) where
 	Lambda x $$ Lambda y = Lambda $ x :^: y
@@ -31,7 +56,7 @@ free = Lambda . Leaf . Free
 abstract :: Lambda (Maybe v) -> Lambda v
 abstract (Lambda t) = Lambda $ Leaf $ Abstraction $ Lambda $ bindWith 1 <$> t
 	where
-		bindWith :: Integer -> LambdaTerm (Maybe v) -> LambdaTerm v
+		bindWith :: Int -> LambdaTerm (Maybe v) -> LambdaTerm v
 		bindWith n (Abstraction (Lambda s)) =
 			Abstraction $ Lambda $ bindWith (n+1) <$> s
 		bindWith _ (Bound i) = Bound i
@@ -46,14 +71,14 @@ variableNames = flip (:) <$> (flip replicate '\'' <$> [0..]) <*> ['a'..'z']
 render :: forall v. (v -> String) -> Lambda v -> String
 render renderVar = renderDepth 0 where
 
-	renderDepth :: Integer -> Lambda v -> String
+	renderDepth :: Int -> Lambda v -> String
 	renderDepth d (Lambda x) = renderL (renderTerm d) x
 
-	renderTerm :: Integer -> LambdaTerm v -> String
+	renderTerm :: Int -> LambdaTerm v -> String
 	-- todo collect nested abstractions and render together
 	renderTerm d (Abstraction l) =
-		"λ" <> genericIndex variableNames d <> "." <> renderDepth (d+1) l
-	renderTerm d (Bound i) = genericIndex variableNames (d-i)
+		"λ" <> variableNames !! d <> "." <> renderDepth (d+1) l
+	renderTerm d (Bound i) = variableNames !! (d-i)
 	renderTerm _ (Free v) = renderVar v
 
 -- Todo substitution
