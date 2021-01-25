dc433742f7c521432dfa4e349bd6df43ad2117af
olligobber <sirolligobbervii@yahoo.com.au>
2021-01-25 22:49:57 +1100

    Added conversions between SKI and lambda calculus

---
+++ LambdaSKI.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module LambdaSKI
+	( toLambda
+	, toSKI
+	) where
+
+import Data.Function (on)
+import Data.Maybe (isNothing)
+
+import Lambda (Lambda(..), LambdaTerm(..), free, abstract, substitute)
+import Reducible (($$))
+import SKI (SKI(..), HasSKI(..))
+import BinaryTree (BinaryTree(..), fromBinaryTree)
+
+toLambda :: SKI -> Lambda v
+toLambda S = abstract $ abstract $ abstract $
+	free (Just (Just Nothing)) $$ free Nothing $$
+	(free (Just Nothing) $$ free Nothing)
+toLambda K = abstract $ abstract $ free $ Just Nothing
+toLambda I = abstract $ free Nothing
+
+-- Checks if a variable occurs bound to an abstraction wrapping the given tree
+isBound :: Lambda a -> Bool
+isBound l = any isNothing $ substitute (Just <$> l) $ free Nothing
+
+toSKI :: forall a. HasSKI a => Lambda a -> BinaryTree a
+toSKI (Lambda (Leaf (Free x))) = Leaf x
+toSKI (Lambda (Leaf (Bound _))) = error "Bound variable with no abstraction"
+toSKI (Lambda (l :^: r)) = ((:^:) `on` toSKI . Lambda) l r
+toSKI (Lambda (Leaf (Abstraction l))) | not $ isBound l = k $$ toSKI l
+toSKI (Lambda (Leaf (Abstraction (Lambda (Leaf (Bound 1)))))) = i
+toSKI (Lambda (Leaf (Abstraction l@(Lambda (Leaf (Abstraction _)))))) =
+	toSKI $ abstract $ fromBinaryTree $ free <$>
+	toSKI (substitute (Just <$> l) $ free Nothing)
+toSKI (Lambda (Leaf (Abstraction (Lambda (l :^: r))))) = s $$
+	toSKI (Lambda $ Leaf $ Abstraction $ Lambda l) $$
+	toSKI (Lambda $ Leaf $ Abstraction $ Lambda r)
+toSKI _ = error "Unreachable option reached"
